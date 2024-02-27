6af93ccec98622a96f486787e9f40e1970439069
Jonas <publik.froeller.jonas@gmx.at>
2024-02-27 08:10:01 +0100

    fix(rest-api): fixed paths

--- src/lib/rest_endpoints.ts
+++ src/lib/rest_endpoints.ts
@@ -2,6 +2,7 @@ import { DEV_MODE } from '$env/static/private';
 
 export const REST_API_BASE_URL =
 	DEV_MODE === 'true' ? 'http://localhost:3000' : 'https://propromo-rest.duckdns.org';
+const REST_API_VERSION = 'v1';
 export const REST_API_MILESTONE_ENDPOINT = function (
 	organization_name: string,
 	project_id: string,
@@ -11,5 +12,5 @@ export const REST_API_MILESTONE_ENDPOINT = function (
 		issue_states?: string | null;
 	}
 ) {
-	return `github/orgs/${organization_name}/projects/${project_id}/repositories/milestones/${milestone_id}?depth=${query.depth}&issue_states=${query.issue_states}`;
+	return `/${REST_API_VERSION}/github/orgs/${organization_name}/projects/${project_id}/repositories/milestones/${milestone_id}?depth=${query.depth}&issue_states=${query.issue_states}`;
 };
