171c9acf021289358752e9111a6da24e907227e8
Jonas <publik.froeller.jonas@gmx.at>
2024-02-27 10:03:07 +0100

    testing(development): added reverse proxy for GithubApp testing in the development environment

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,26 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import { loadEnv } from 'vite';
+import { ngrok } from 'vite-plugin-ngrok';
+
+const VITE_NGROK_AUTH_TOKEN = loadEnv('development', process.cwd(), [
+	'VITE_'
+]).VITE_NGROK_AUTH_TOKEN; // import.meta.env.VITE_NGROK_AUTH_TOKEN;
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+		sveltekit(),
+		ngrok({
+			authtoken: VITE_NGROK_AUTH_TOKEN
+		})
+	],
 	server: {
 		port: 5000,
-		strictPort: false
+		strictPort: false,
+		hmr: {
+			// use reverse proxy with vite hmr
+			clientPort: 443
+		}
 	},
 	preview: {
 		port: 6000,
