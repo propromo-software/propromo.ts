6ce75ece954929889f116907903722847455977b
Jonas <publik.froeller.jonas@gmx.at>
2024-03-02 21:48:50 +0100

    fix(rest-api): updated host url (closes #5)

--- src/lib/rest_endpoints.ts
+++ src/lib/rest_endpoints.ts
@@ -1,7 +1,7 @@
 import { DEV_MODE } from '$env/static/private';
 
 export const REST_API_BASE_URL =
-	DEV_MODE === 'true' ? 'http://localhost:3000' : 'https://propromo-rest.duckdns.org';
+	DEV_MODE === 'true' ? 'http://localhost:3000' : 'https://rest-microservice.onrender.com'; // https://propromo-rest.duckdns.org
 const REST_API_VERSION = 'v1';
 export const REST_API_MILESTONE_ENDPOINT = function (
 	organization_name: string,
